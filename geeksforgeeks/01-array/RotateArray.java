43d3f0bf4ff3cc312ff1acd8612288972a7b08c7
choongh2e <choonghee.lee@hotmail.com>
2022-04-26 09:34:31 +0900

    geeksforgeeks: rotate array one by one

--- geeksforgeeks/01-array/RotateArray.java
+++ geeksforgeeks/01-array/RotateArray.java
@@ -21,19 +21,39 @@ public class RotateArray {
     }
   }
 
+  void rotateOneByOne(int arr[], int d, int n)
+  {
+    // d times
+    for(int j=0; j<d; j++) {
+
+      // rotate one by one
+      int temp = arr[0];
+      for(int i=0; i< n - 1; i++) 
+      {
+        arr[i] = arr[i+1];
+      }
+      arr[n-1] = temp;
+    }
+  }
+
   void printArray(int arr[], int size)
   {
     for(int i = 0; i < size; i++)
     {
       System.out.print(arr[i] + " ");
     }
+    System.out.println();
   }
 
   public static void main(String[] args)
   {
     RotateArray ra = new RotateArray();
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    ra.rotateWithTempArray(arr, 2, 7);
-    ra.printArray(arr, 7);
+
+    ra.rotateWithTempArray(arr, 2, 7); 
+    ra.printArray(arr, 7); // 3 4 5 6 7 1 2 
+
+    ra.rotateOneByOne(arr, 3, 7);
+    ra.printArray(arr, 7); // 6 7 1 2 3 4 5
   }
 }
