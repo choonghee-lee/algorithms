c0bfc9a2dde8f7296da4e18b30f6a0b8d603332a
choongh2e <choonghee.lee@hotmail.com>
2022-07-07 10:29:18 +0900

    leetcode: P21 Merge Two Sorted Lists

---
+++ leetcode/easy/P21_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,18 @@
+public class P21_Merge_Two_Sorted_Lists {
+    public ListNode mergeTwoLists(ListNode node1, ListNode node2) {
+
+        if(node1 == null) return node2;
+        if(node2 == null) return node1;
+
+        if(node1.val < node2.val)
+        {
+            node1.next = mergeTwoLists(node1.next, node2);
+            return node1;
+        }
+        else
+        {
+            node2.next = mergeTwoLists(node1, node2.next);
+            return node2;
+        }
+    }
+}
