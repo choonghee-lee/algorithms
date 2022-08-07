267ffb03ca6da1552a88548944175d1eb6d28e5b
choongh2e <choonghee.lee@hotmail.com>
2022-08-07 09:49:18 +0900

    programmers: 서울에서 김서방 찾기

---
+++ programmers/level1/서울에서김서방찾기.java
@@ -0,0 +1,18 @@
+class 서울에서김서방찾기 {
+    public String solution(String[] seoul) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("김서방은 ");
+        for(int i = 0; i < seoul.length; i++)
+        {
+            if(seoul[i].equals("Kim"))
+            {
+                sb.append(i);
+                break;
+            }
+        }
+        sb.append("에 있다");
+
+        return sb.toString();
+    }
+}
